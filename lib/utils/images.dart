abbc2e57a8f6c257cd0a8797670d5a1af3d8ab05
AizanSagheer <m.aizan.scheena@gmail.com>
2025-12-26 11:19:28 +0400

    skills/potfolio images added

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -4,12 +4,18 @@ class AppImages {
   static const String linkedin = 'assets/images/linkedin.svg';
   static const String stackoverflow = 'assets/images/stackoverflow.svg';
   static const String aizan = 'assets/images/aizan.png';
+  static const String aizanSmiling = 'assets/images/smiling.png';
   static const String flutterLogo = 'assets/images/flutter_logo.svg';
   static const String package = 'assets/images/package.svg';
   static const String firebase = 'assets/images/firebase.svg';
   static const String git = 'assets/images/git.svg';
   static const String pipeline = 'assets/images/pipeline.svg';
   static const String debug = 'assets/images/debugging.svg';
+  static const String fastApi = 'assets/images/fastApi.svg';
+  static const String gemini = 'assets/images/gemini.svg';
+  static const String kotlin = 'assets/images/kotlin.svg';
+  static const String postgresql = 'assets/images/postgresql.svg';
+  static const String python = 'assets/images/python.svg';
 
   static const String education = 'assets/images/education.svg';
   static const String food = 'assets/images/food.svg';
@@ -20,4 +26,17 @@ class AppImages {
   static const String wallet = 'assets/images/wallet.svg';
   static const String wellness = 'assets/images/wellness.svg';
   static const String library = 'assets/images/library.svg';
+
+  // App Icons (PNG images - download from app stores)
+  static const String astriaDigitalLibrary = 'assets/images/app_icons/astria_digital_library.png';
+  static const String acmsStudentApp = 'assets/images/app_icons/acms_student_app.png';
+  static const String afrikchat = 'assets/images/app_icons/afrikchat.png';
+  static const String afrikwaveUser = 'assets/images/app_icons/afrikwave_user.png';
+  static const String afrikwaveSeller = 'assets/images/app_icons/afrikwave_seller.png';
+  static const String ringOfWell = 'assets/images/app_icons/ring_of_well.png';
+  static const String zelatofx = 'assets/images/app_icons/zelatofx.png';
+  static const String cityGoldBullion = 'assets/images/app_icons/city_gold_bullion.png';
+  static const String alSabayek = 'assets/images/app_icons/al_sabayek.png';
+  static const String ceekay = 'assets/images/app_icons/ceekay.png';
+  static const String ang = 'assets/images/app_icons/ang.png';
 }
